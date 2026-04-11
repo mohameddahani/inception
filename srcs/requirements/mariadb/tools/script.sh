521f28dfbf3c371dc07e8f0ed3a8dfb7ff8b8441
Mohamed Dahani <mohamedxdahani@gmail.com>
2026-04-11 15:21:57 +0100

    finally mariadb is work and i switch to debian as base image

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -1,13 +1,36 @@
-#!/bin/sh
+#!/bin/bash
 
-# set -e
+# The set -e command is a setting used in shell scripts (like bash or sh) that tells the script to exit immediately if any command fails.
+set -e
 
-# Add data directory for data bases
-# --user=mysql => add permisssion for mysql to write on directory of databses
-# --ldata=/var/lib/mysql => Initialize database system tables as the mysql user
-mkdir -p /var/lib/mysql && mysql_install_db --user=mysql --ldata=/var/lib/mysql
+echo "Starting MariaDB initialization..."
 
-# CMD defines the default command that runs when a container starts
-# mariadbd-safe ==> this command start MariaDB server as the main process inside the container
-# --datadir=/var/lib/mysql ==> this command tell the mariadb where he should found database system tables
-exec mariadbd-safe && --datadir=/var/lib/mysql
+# Start MariaDB
+service mariadb start
+
+# Wait until MariaDB is ready
+until mysqladmin ping --silent; do
+    sleep 1
+done
+
+# Create database and user
+# '%' => allows the user to connect from ANY host (any container / any IP)
+# CREATE DATABASE => creates the database if it does not already exist
+# CREATE USER => creates a new MySQL user with a password
+# GRANT ALL PRIVILEGES => gives the user full permissions (SELECT, INSERT, UPDATE, DELETE, etc.)
+# on all tables (*) inside the database (my_wordpress_db)
+# FLUSH PRIVILEGES => reloads the privilege tables so changes take effect immediately
+mariadb -u root << MYSQL_QUERIES
+CREATE DATABASE IF NOT EXISTS my_wordpress_db;
+CREATE USER IF NOT EXISTS 'my_wordpress_usr'@'%' IDENTIFIED BY '1234';
+GRANT ALL PRIVILEGES ON my_wordpress_db.* TO 'my_wordpress_usr'@'%';
+FLUSH PRIVILEGES;
+MYSQL_QUERIES
+
+echo "MariaDB initialization complete."
+
+# Stop service (clean shutdown)
+service mariadb stop
+
+# Start MariaDB as main process (PID 1)
+exec mariadbd --user=mysql --datadir=/var/lib/mysql
