f36a1ec3547c30282c7228d17eb2213ac93557cb
Mohamed Dahani <mohamedxdahani@gmail.com>
2026-04-18 14:10:35 +0100

    add some comments

--- srcs/requirements/bonus/adminer/tools/script.sh
+++ srcs/requirements/bonus/adminer/tools/script.sh
@@ -6,4 +6,7 @@ set -e
 # Print message
 echo "Starting Adminer..."
 
+# php => Runs the PHP interpreter.
+# -S => Starts PHP’s built-in web server.
+# 0.0.0.0:8081 => Binds the server to all network interfaces on port 8081 (accessible outside localhost, e.g. Docker or other devices).
 exec php -S 0.0.0.0:8081
